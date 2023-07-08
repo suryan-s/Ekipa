dfa2ecc8b5697620864e9dccb6e2ca444ee43940
Arjun S <arjun8107@gmail.com>
2023-07-08 20:57:46 +0530

    fix(fe): myteam card

--- frontend/src/components/myteam/MemberCard.tsx
+++ frontend/src/components/myteam/MemberCard.tsx
@@ -5,31 +5,57 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { DotsVerticalIcon } from "@radix-ui/react-icons";
 
-export default function MemberCard() {
+export default function MemberCard({
+  name,
+  tasksOnProgress,
+  pendingTasks,
+  backlogs,
+}: {
+  name: string;
+  tasksOnProgress: number;
+  pendingTasks: number;
+  backlogs: number;
+}) {
   return (
     <Card className="relative">
       <CardHeader className="flex-row items-center gap-3">
         <Avatar>
-          <AvatarFallback>SS</AvatarFallback>
+          <AvatarFallback>{name.slice(0, 2)}</AvatarFallback>
         </Avatar>
-        <CardTitle>Card Title</CardTitle>
+        <CardTitle>{name}</CardTitle>
       </CardHeader>
       <CardContent>
         <p>Tasks on progress:</p>
-        <p className="text-2xl">5</p>
+        <p className="text-2xl">{tasksOnProgress}</p>
       </CardContent>
       <CardFooter className="flex-col items-start">
         <div className="flex gap-3">
           <p>Pending tasks:</p>
-          <p>1</p>
+          <p>{pendingTasks}</p>
         </div>
         <div className="flex gap-3">
           <p>Backlogs:</p>
-          <p>3</p>
+          <p>{backlogs}</p>
         </div>
       </CardFooter>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="absolute hover:bg-slate-900 p-3 rounded-full top-0 right-0 m-5">
+          <DotsVerticalIcon />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Remove</DropdownMenuItem>
+          <DropdownMenuItem>Message</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Card>
   );
 }
--- frontend/src/components/myteam/MyTeam.tsx
+++ frontend/src/components/myteam/MyTeam.tsx
@@ -3,7 +3,18 @@ import MemberCard from "./MemberCard";
 export default function MyTeam() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mt-6">
-      <MemberCard />
+      <MemberCard
+        name="Suryan"
+        tasksOnProgress={5}
+        pendingTasks={1}
+        backlogs={4}
+      />
+      <MemberCard
+        name="Akash"
+        tasksOnProgress={5}
+        pendingTasks={1}
+        backlogs={4}
+      />
     </div>
   );
 }
