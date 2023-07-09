805b1aedd163df42fd76df6fad892dc8b0829bff
Arjun S <arjun8107@gmail.com>
2023-07-09 12:44:04 +0530

    feat(fe): possible fix

--- frontend/src/components/tasks/Tasks.tsx
+++ frontend/src/components/tasks/Tasks.tsx
@@ -4,26 +4,27 @@ import { Task, taskSchema } from "./data/schema";
 import { useContext, useEffect, useState } from "react";
 import { z } from "zod";
 import { AuthContext } from "@/context/AuthContext";
-async function getData(abortController: AbortController, setToken?: any) {
-  const res = await fetch("http://localhost:8000/task/allTaskList", {
-    signal: abortController.signal,
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
-  if (res.status === 401) {
-    if (setToken) setToken(null);
-  }
-  const tasks = await res.json();
-  console.log(tasks);
-  tasks.data.forEach((task: any) => {
-    task.priority = task.priority.toString();
-  });
-  return z.array(taskSchema).parse(tasks.data);
-}
+
 export default function Tasks() {
   const [data, setData] = useState<Task[]>([]);
-  const { setToken } = useContext(AuthContext);
+  const { token, setToken } = useContext(AuthContext);
+  async function getData(abortController: AbortController) {
+    const res = await fetch("http://localhost:8000/task/allTaskList", {
+      signal: abortController.signal,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (res.status === 401) {
+      if (setToken) setToken(null);
+    }
+    const tasks = await res.json();
+    console.log(tasks);
+    tasks.data.forEach((task: any) => {
+      task.priority = task.priority.toString();
+    });
+    return z.array(taskSchema).parse(tasks.data);
+  }
   useEffect(() => {
     const abortController = new AbortController();
     getData(abortController, setToken).then(setData);
