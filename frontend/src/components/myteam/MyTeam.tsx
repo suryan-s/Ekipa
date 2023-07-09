72278d4f19b34dfabb0f46acb697a1385fe47cfb
Arjun S <arjun8107@gmail.com>
2023-07-09 09:03:43 +0530

    fix(fe): fetch data for myteam

--- frontend/src/components/myteam/MyTeam.tsx
+++ frontend/src/components/myteam/MyTeam.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import MemberCard from "./MemberCard";
 
 export default function MyTeam() {
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch("http://localhost:8000/user/teamMembers", {
+      signal: abortController.signal,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mt-6">
       <MemberCard
--- frontend/src/components/teams/Teams.tsx
+++ frontend/src/components/teams/Teams.tsx
@@ -2,10 +2,19 @@ import { DataTable } from "./dataTable/DataTable";
 import { columns } from "./data/columns";
 import { Task, taskSchema } from "./data/schema";
 import { useEffect, useState } from "react";
-import { tasks } from "./data/tasks";
 import { z } from "zod";
 async function getData() {
-  return z.array(taskSchema).parse(tasks);
+  const res = await fetch("http://localhost:8000/user/allTeamDetails", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+  return z.array(taskSchema).parse(res);
 }
 export default function Teams() {
   const [data, setData] = useState<Task[]>([]);
